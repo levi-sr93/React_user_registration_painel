9c0d752ffbd4e63d192d4a6e8b349d7ff18e361d
Levi Santos R <levi-sr@outlook.com>
2019-11-15 17:07:01 -0300

    added CRUD functions and some CSS styling

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,16 +1,16 @@
 import './Nav.css';
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 export default props =>
     <aside className="menu-area">
         <nav className="menu">
-            <a href="#/">
+            <Link to="/">
                 <i className="fa fa-home"></i> Home
-            </a>
+            </Link>
 
-            <a href="#/users">
+            <Link to="/users">
                 <i className="fa fa-users"></i> Users
-            </a>
+            </Link>
         </nav>
     </aside>
